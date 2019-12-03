00a05367bdb6a894df357233b039b46b31f4832a
Richard Cheung <richardcheung1999@gmail.com>
2019-12-03 00:34:29 +0000

    Updated Miniproject

--- Mini Project/PokemonSimulator.java
+++ Mini Project/PokemonSimulator.java
@@ -1,133 +1,256 @@
+/**
+ *
+ *
+ */
 import java.util.Scanner;
 
 class PokemonSimulator {
-	
-	public static void main (String[] p) {
-		title();
-		playerMode();
-		battle();
-	} // end main
-	
-	// Start of program, with choice of whether a new save is created or an existing one is loaded.	
-	public static void title() {
-		print("*****************************\n*   NEW GAME     CONTINUE   *\n*****************************");
-		String choice = input("");
-		if (choice.matches("NEW GAME|CONTINUE")) {
-			if (choice.matches("NEW GAME")) { } // Return to main
-			else { // choice.matches("CONTINUE")
-				unprogram();
-			}
-		}
-		else {
-			error();
-			title();
-		}
-	} // end title
-	
-	// Choice of whether to battle against an AI or a second local player.
-	public static void playerMode() {
-		print("*****************************\n*   VS CPU	 VS HUMAN   *\n*****************************");
-		String choice = input("");
-		if (choice.matches("VS CPU|VS HUMAN")) {
-			if (choice.matches("VS CPU")) {
-				unprogram();
-			}
-			else { } // choice.matches("VS CPU)" ; Return to main
-		}
-		else {
-			error();
-			playerMode();
-		}
-	} // end playerMode
-	
-	// Displays the battle menu
-	public static void battle() {
-		print("*****************************\n*   FIGHT\t  POKEMON   *\n*    BAG\t   SAVE     *\n*****************************");
-		String choice = input("");
-			if (choice.matches("FIGHT|POKEMON|BAG|SAVE")) {
-				if (choice.matches("FIGHT")) {
-					//fight();
-					unprogram();
-				}
-				else if (choice.matches("POKEMON")) {
-					//team();
-					unprogram();
-				}
-				else if (choice.matches("BAG")) {
-					items();
-				}
-				else { //choice.matches("SAVE")
-					//save();
-					unprogram();					
-				}
-			}
-			else {
-				error();
-				battle();
-			}
-	} // end battle
-	
-	public static void items() {
-		String bag[] = { "MAX POTION", "REVIVE", "POTION", "SHINY STONE", "MAX REVIVE", "SUPER POTION" };
-		
-		print("ITEM BAG\n");
-		for (int i = 0; i < bag.length; i++) {
-			print(bag[i]);
-		}
-		print("*****************************\n*    USE    SORT    BACK    *\n*****************************");
-	}
-	
-	
-	/*	METHODS USED TO SIMPLIFY CODE BY USING KEYWORDS	*/
-	
-	// A generic error message to be printed if an input is not valid.
-	public static void error() {
-		print("THIS IS NOT A VALID INPUT");
-	}
-	
-	// A generic message to be printed if an option that has not yet been programmed is selected.
-	// The program will be forced to terminate.
-	public static void unprogram() {
-		print("*****************************************************\nTHIS FEATURE HAS NOT BEEN IMPLEMENTED IN THIS VERSION\n\t       PLEASE CHECK BACK SOON!\n\t   THE PROGRAM WILL NOW TERMINATE!\n*****************************************************");
-		System.exit(0);
-	}
-	
-	// Ask for a string with message ">>> CHOOSE AN OPTION"; will overwrite any message printed in the input.
+
+    public static void main(String[] p) {
+        title();
+        playerMode();
+        battle();
+    } // End method main
+
+    // Start of program, with choice of whether a new save is created or an existing one is loaded.
+    public static void title() {
+        print("*****************************\n*   NEW GAME     CONTINUE   *\n*****************************");
+        String choice = input("");
+        if (choice.matches("NEW GAME|CONTINUE")) {
+            if (choice.matches("NEW GAME")) { // Return to main
+            } else { // choice.matches("CONTINUE")
+                unprogram();
+                title();
+            }
+        } else {
+            error();
+            title();
+        }
+    } // End method title
+
+    // Choice of whether to battle against an AI or a second local player.
+    public static void playerMode() {
+        print("*****************************\n*   VS CPU       VS HUMAN   *\n*****************************");
+        String choice = input("");
+        if (choice.matches("VS CPU|VS HUMAN")) {
+            if (choice.matches("VS CPU")) {
+                unprogram();
+                playerMode();
+            } // else choice.matches("VS HUMAN") ; Return to main
+        } else {
+            error();
+            playerMode();
+        }
+    } // End method playerMode
+
+    // Displays the battle menu
+    public static void battle() {
+        // Setting abstract data type
+        ally a1  = createAlly ("TORTERRA" , 75, 239, 155, 159, 101);
+        ally a2  = createAlly ("INFERNAPE", 75, 211, 173, 123, 179);
+        ally a3  = createAlly ("EMPOLEON" , 75, 223, 165, 159, 107);
+        ally a4  = createAlly ("CLEFABLE" , 75, 239, 141, 139, 107);
+        ally a5  = createAlly ("ALAKAZAM" , 75, 179, 156, 122, 101);
+        ally a6  = createAlly ("MAGMORTAR", 75, 209, 182, 138, 141);
+        enemy e1 = createEnemy("SPIRITOMB", 74, 169, 153, 178, 68 );
+        enemy e2 = createEnemy("ROSERADE" , 74, 184, 161, 143, 102);
+        enemy e3 = createEnemy("TOGEKISS" , 76, 228, 147, 177, 139);
+        enemy e4 = createEnemy("LUCARIO"  , 76, 203, 187, 123, 153);
+        enemy e5 = createEnemy("MILOTIC"  , 74, 236, 135, 168, 136);
+        enemy e6 = createEnemy("GARCHOMP" , 78, 268, 180, 157, 176);
+
+        print("*****************************\n*   FIGHT\t  POKEMON   *\n*    BAG\t   SAVE     *\n*****************************");
+        String choice = input("");
+        if (choice.matches("FIGHT|POKEMON|BAG|SAVE")) {
+            if (choice.matches("FIGHT")) {
+                fight();
+            } else if (choice.matches("POKEMON")) {
+                team(a1, a2, a3, a4, a5, a6);
+            } else if (choice.matches("BAG")) {
+                items();
+            } else { //choice.matches("SAVE")
+                //save();
+                unprogram();
+                battle();
+            }
+        } else {
+            error();
+            battle();
+        }
+    } // End method battle
+
+    public static void fight() {
+        unprogram();
+        // Damage formula
+        //int DAMAGE = (2*LEVEL/5+2)*40*ATK/DEF/50+2;
+        battle();
+    }
+
+    // lists ally team information with level, name and health
+    public static void team(ally a1, ally a2, ally a3, ally a4, ally a5, ally a6) {
+        print("------- YOUR POKEMON --------");
+        print(teamDetail(a1) + "\n" + teamDetail(a2) + "\n" + teamDetail(a3) + "\n" + teamDetail(a4) + "\n" + teamDetail(a5) + "\n" + teamDetail(a6));
+        battle();
+    }
+
+    // lists items in the bag
+    public static void items() {
+        String bag[] = {"MAX POTION", "REVIVE", "POTION", "SHINY STONE", "MAX REVIVE", "SUPER POTION"};
+        String choice = "";
+
+        print("------------ BAG ------------");
+        for (int i = 0; i < bag.length; i++) {
+            print("\t" + bag[i]);
+        }
+        print("*****************************\n*    USE    SORT    BACK    *\n*****************************");
+        choice = input("");
+        if (choice.matches("USE|SORT|BACK")) {
+            if (choice.matches("USE")) {
+                unprogram();
+                items();
+            } else if (choice.matches("SORT")) {
+                sortItems(bag);
+            } else { // choice.matches("BACK");
+                battle();
+            }
+        }
+    } // End method items
+
+    // Method that sorts the bag in alphabetical order
+    public static void sortItems(String[] bag) {
+        print("----- BAG ALPHABETICAL ------");
+        String temp;
+        for (int j = 0; j < bag.length; j++) {
+       	    for (int i = j + 1; i < bag.length; i++) {
+    		    // Compares adjacent strings
+        		if (bag[i].compareTo(bag[j]) < 0) {
+        			temp = bag[j];
+        			bag[j] = bag[i];
+        			bag[i] = temp;
+        		}
+    	    }
+    	   System.out.println("\t" + bag[j]);
+    	}
+        items();
+    } // End method sortItems
+
+    /**
+     * METHODS USED TO SIMPLIFY CODE BY USING KEYWORDS
+     */
+
+    // A generic error message to be printed if an input is not valid.
+    public static void error() {
+        print("THIS IS NOT A VALID INPUT");
+    }
+
+    // A generic message to be printed if an option that has not yet been programmed is selected.
+    // The program will be forced to terminate.
+    public static void unprogram() {
+        print("*****************************\n* THIS FEATURE HAS NOT BEEN *\n*IMPLEMENTED IN THIS VERSION*\n*****************************");
+    }
+
+    // Ask for a string with message ">>> CHOOSE AN OPTION"; will overwrite any message printed in the input.
     // toUpperCase forces an input to be capitals, allowing lowercase input by the user.
-	public static String input(String message) {
-		Scanner scan = new Scanner(System.in);
-		print(">>> CHOOSE AN OPTION");
-		String userInput = scan.nextLine();
-		return userInput.toUpperCase();
-	}
+    public static String input(String message) {
+        Scanner scan = new Scanner(System.in);
+        print(">>> CHOOSE AN OPTION");
+        String userInput = scan.nextLine();
+        return userInput.toUpperCase();
+    }
 
-	// Print message to the screen
-	public static void print(String message) {
-		System.out.println(message);
-	}
-} // end class PokemonSimulator 
+    // Print message to the screen
+    public static void print(String message) {
+        System.out.println(message);
+    }
 
-/* GUI
-*****************************
-*   NEW GAME     CONTINUE   *
-*****************************
+    /**
+     * ADT METHODS
+     */
 
-*****************************
-*   VS CPU		 VS HUMAN   *
-*****************************
+    // Creating ally abstract data type
+    public static ally createAlly(String allyName, int allyLV, int allyHP, int allyATK, int allyDEF, int allySPD) {
+        ally a = new ally();
+        a.name = allyName;
+        a.LV = allyLV;
+        a.HP = allyHP;
+        a.ATK = allyATK;
+        a.DEF = allyDEF;
+        a.SPD = allySPD;
+        return a;
+    }
 
-*****************************
-*   FIGHT         POKEMON   *
-*    BAG           SAVE     *
-*****************************
+    // Creating enemy abstract data type
+    public static enemy createEnemy(String enemyName, int enemyLV, int enemyHP, int enemyATK, int enemyDEF, int enemySPD) {
+        enemy e = new enemy();
+        e.name = enemyName;
+        e.LV = enemyLV;
+        e.HP = enemyHP;
+        e.ATK = enemyATK;
+        e.DEF = enemyDEF;
+        e.SPD = enemySPD;
+        return e;
+    }
 
-*****************************
-*    USE    SORT    BACK    *
-*****************************
-*****************************\n*    USE    SORT    BACK    *\n*****************************
-*****************************************************
-THIS FEATURE HAS NOT BEEN IMPLEMENTED IN THIS VERSION
-               PLEASE CHECK BACK SOON!
-           THE PROGRAM WILL NOW TERMINATE!
-*****************************************************
-*/
+    // Accessor methods for ally and enemy ADT
+    public static String getAllyName  (ally a)  { return a.name; }
+    public static int    getAllyLV    (ally a)  { return a.LV;   }
+    public static int    getAllyHP    (ally a)  { return a.HP;   }
+    public static int    getAllyATK   (ally a)  { return a.ATK;  }
+    public static int    getAllyDEF   (ally a)  { return a.DEF;  }
+    public static int    getAllySPD   (ally a)  { return a.SPD;  }
+
+    public static String getEnemyName (enemy e) {return e.name;  }
+    public static int    getEnemyLV   (enemy e) {return e.LV;    }
+    public static int    getEnemyHP   (enemy e) {return e.HP;    }
+    public static int    getEnemyATK  (enemy e) {return e.ATK;   }
+    public static int    getEnemyDEF  (enemy e) {return e.DEF;   }
+    public static int    getEnemySPD  (enemy e) {return e.SPD;   }
+
+    // Returns a readable string
+    public static String teamDetail(ally a) {
+        return "Lv." + getAllyLV(a) + "\t" + getAllyName(a) + "\t" + getAllyHP(a) + "HP";
+    }
+} // End class PokemonSimulator
+
+// Abstract Data Type classes
+class ally {
+    String name;
+    int LV;
+    int HP;
+    int ATK;
+    int DEF;
+    int SPD;
+}
+
+class enemy {
+    String name;
+    int LV;
+    int HP;
+    int ATK;
+    int DEF;
+    int SPD;
+}
+
+/**
+ * GUI
+ * ****************************
+ * NEW GAME     CONTINUE   *
+ * ****************************
+ * ****************************
+ * VS CPU       VS HUMAN   *
+ * ****************************
+ * ****************************
+ * FIGHT         POKEMON   *
+ * BAG           SAVE     *
+ * ****************************
+ * ------- YOUR POKEMON --------
+ * ------------ BAG ------------
+ * ----- BAG ALPHABETICAL ------
+ * ****************************
+ * USE    SORT    BACK    *
+ * ****************************
+ * ****************************
+ * THIS FEATURE HAS NOT BEEN *
+ * IMPLEMENTED IN THIS VERSION*
+ * ****************************
+ */
